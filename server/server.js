7b43067d381d523620fabffd3449d19551138751
SK Shivaanee <shivaaneesk@gmail.com>
2025-10-30 08:21:35 +0530

    remove cors

--- server/server.js
+++ server/server.js
@@ -23,11 +23,7 @@ connectDB();
 
 const app = express();
 
-const corsOptions = {
-  origin: 'http://localhost:5173',
-  optionsSuccessStatus: 200
-};
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json({ limit: "10mb" }));
 
